fe7000db1c7d49990bad1de1a64cc0306c1d9e6b
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2021-11-22 16:53:11 +0900

    딥러닝에서 쓰는 용어 추가

--- python/교재 예제/chap10/(p.339)grad_descent.py
+++ python/교재 예제/chap10/(p.339)grad_descent.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 X = np.array([0.0, 1.0, 2.0])
 y = np.array([3.0, 3.5, 5.5])
 
-W = 0 # 기울기
-b = 0 # 절편
+W = 0 # 기울기(가중치)
+b = 0 # 절편(바이어스)
 
 lrate = 0.01 # 학습률
 epochs = 1000 # 반복 횟수
