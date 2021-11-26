fb77a35d2337f03fec726463b9b902a18c268420
ghk01214 <ghk01214@naver.com>
2021-11-27 05:59:42 +0900

    체력이 0이하일 때 체력을 초기화 시키는 것으로 변경

--- Raven/Raven_SensoryMemory.cpp
+++ Raven/Raven_SensoryMemory.cpp
@@ -250,8 +250,8 @@ void Raven_SensoryMemory::UpdateEnemyHealth(const int pOpponentID, int iDamage)
 		// 피격당한 봇일 경우
 		if ((*iter).first->ID() == pOpponentID)
 		{
-			// 봇의 체력이 0인 경우 100으로 다시 채운다
-			if (!(*iter).second.iEnemyHealth)
+			// 봇의 체력이 0 이하인 경우 100으로 다시 채운다
+			if ((*iter).second.iEnemyHealth <= 0)
 			{
 				(*iter).second.iEnemyHealth = 100;
 			}
